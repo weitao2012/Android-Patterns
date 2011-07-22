5d044c495a8f83a92e521f4ab392eb763e4a98fe
Klevison Matias <klevison@gmail.com>
2011-07-21 23:36:55 -0300

    SegmentedRadioButtons for Android

---
+++ src/worq/patterns/Patterns.java
@@ -0,0 +1,13 @@
+package worq.patterns;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Patterns extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
